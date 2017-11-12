e30e5882d65fea6381901c2ca13869f319cda507
Robin van het Hof <robin.van.het.hof@qualiture.nl>
2017-11-12 11:30:26 +0100

    Added SnapshotViewController

--- ESPMReminders/SnapshotViewController.swift
+++ ESPMReminders/SnapshotViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 // This ViewController is used as a snapshot screen when the app is put to the background
 class SnapshotViewController: UIViewController {
-
+    
     private var imageView: UIImageView!
-
+    
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor.white
-
+        
         let sapLogo = UIImage(named: "SAPLogo.png")!
         self.imageView = UIImageView(image: sapLogo)
         self.imageView.center = self.view.center
-
+        
         self.view.addSubview(imageView)
     }
 }
+
